398ccfe8c2c2580e5aef9737e5f2aa8450507a32
AndresFE0209 <afernandeze10@iesalbarregas.es>
2026-02-18 11:45:59 +0100

    subida clases para widgtes y colores

---
+++ mobile_app/lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color verde = Color(0xFF007A3D);
+  static const Color verdeClaro = Color(0xFF4CAF70);
+  static const Color blanco = Color(0xFFFFFFFF);
+  static const Color grisNeutro = Color(0xFF7A8587);
+  static const Color negroSuave = Color(0xFF2F3333);
+  static const Color negro = Color(0xFF0B0B0B);
+
+  static const Color inputBackground = Color(0xFF3F5F1);
+
+}
